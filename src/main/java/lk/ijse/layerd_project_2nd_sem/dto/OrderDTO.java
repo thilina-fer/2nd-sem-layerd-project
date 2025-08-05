49fd967ef5b36848e76694a4257f83830c7be284
thilina-fer <dilshanthilina1010@gmail.com>
2025-08-05 07:01:21 +0530

    create the orderDTO class

---
+++ src/main/java/lk/ijse/layerd_project_2nd_sem/dto/OrderDTO.java
@@ -0,0 +1,15 @@
+package lk.ijse.layerd_project_2nd_sem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class OrderDTO {
+    private String orderId;
+    private String customerContact;
+    private String date;
+}
