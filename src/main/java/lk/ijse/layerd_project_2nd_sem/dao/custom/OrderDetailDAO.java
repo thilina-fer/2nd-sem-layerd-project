74271fe925f71165beec1695b2b46f10e7dae8ac
thilina-fer <dilshanthilina1010@gmail.com>
2025-08-05 07:31:46 +0530

    create the OrderDetailDAO interface

---
+++ src/main/java/lk/ijse/layerd_project_2nd_sem/dao/custom/OrderDetailDAO.java
@@ -0,0 +1,7 @@
+package lk.ijse.layerd_project_2nd_sem.dao.custom;
+
+import lk.ijse.layerd_project_2nd_sem.dao.CrudDAO;
+import lk.ijse.layerd_project_2nd_sem.entity.OrderDetail;
+
+public interface OrderDetailDAO extends CrudDAO<OrderDetail> {
+}
