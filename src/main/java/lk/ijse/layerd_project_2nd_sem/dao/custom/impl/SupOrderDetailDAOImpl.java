f071490f51a1d0551075ab9ed125f5c2e73d95d3
thilina-fer <dilshanthilina1010@gmail.com>
2025-08-10 18:00:28 +0530

    create a supOrderDetailDAOimpl class

---
+++ src/main/java/lk/ijse/layerd_project_2nd_sem/dao/custom/impl/SupOrderDetailDAOImpl.java
@@ -0,0 +1,56 @@
+package lk.ijse.layerd_project_2nd_sem.dao.custom.impl;
+
+import lk.ijse.layerd_project_2nd_sem.dao.SQLUtil;
+import lk.ijse.layerd_project_2nd_sem.dao.custom.SupOrderDetailDAO;
+import lk.ijse.layerd_project_2nd_sem.entity.OrderDetail;
+import lk.ijse.layerd_project_2nd_sem.entity.SupOrderDetails;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class SupOrderDetailDAOImpl implements SupOrderDetailDAO {
+    @Override
+    public ArrayList<OrderDetail> getAll() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean save(OrderDetail entity) throws SQLException, ClassNotFoundException {
+        return SQLUtil.executeUpdate("INSERT INTO sup_order_details (sup_order_id, item_id, qty, unit_price) VALUES (?, ?, ?, ?)",
+                entity.getOrderId(),
+                entity.getItemId(),
+                entity.getQuantity(),
+                entity.getUnitPrice()
+        );
+    }
+
+    @Override
+    public boolean update(OrderDetail entity) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String generateNewId() throws SQLException, ClassNotFoundException {
+        return "";
+    }
+
+    @Override
+    public ArrayList<OrderDetail> search(String searchText) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public OrderDetail find(String contact) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean exist(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+}
